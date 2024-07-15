41fc3cc4131b82f4984f5d05b5ec38ec8b42982b
chessai <chessai1996@gmail.com>
2024-07-15 15:02:23 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,48 +1,75 @@
-# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
-#
-# SPDX-License-Identifier: CC0-1.0
-
 {
-  description = "My haskell application";
+  description = "Static Language Server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+    hs-nix-infra = {
+      url = "github:kadena-io/hs-nix-infra";
+      inputs.hackage.follows = "hackage";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, hs-nix-infra, flake-utils, ... }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "x86_64-darwin"
+        "aarch64-linux" "aarch64-darwin" ] (system:
+    let
+      inherit (hs-nix-infra) nixpkgs haskellNix;
+      pkgs = import nixpkgs {
+        inherit system overlays;
+        inherit (haskellNix) config;
+      };
+      project = pkgs.static-ls;
+      flake = project.flake {
+        # crossPlatforms = p: [ p.ghcjs ];
+      };
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          static-ls =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc964";
+              shell.tools = {
+                cabal = {};
+                haskell-language-server = {};
+                # hlint = {};
+              };
+              shell.buildInputs = with pkgs; [
+                sqlite
+                ghcid
+                hpack
+                pkg-config
+              ];
+              # shell.crossPlatforms = p: [ p.ghcjs ];
+            };
+        })
+      ];
+      # This package depends on other packages at buildtime, but its output does not
+      # depend on them. This way, we don't have to download the entire closure to verify
+      # that those packages build.
+      mkCheck = name: package: pkgs.runCommand ("check-"+name) {} ''
+        echo ${name}: ${package}
+        echo works > $out
+      '';
+    in rec {
+      packages.default = flake.packages."static-ls:exe:static-ls";
+      packages.recursive = with hs-nix-infra.lib.recursive system;
+        wrapRecursiveWithMeta "static-ls" "${wrapFlake self}.default";
 
-        haskellPackages = pkgs.haskell.packages.ghc963;
+      inherit (flake) devShell;
 
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+      packages.check = pkgs.runCommand "check" {} ''
+        echo ${mkCheck "static-ls" packages.default}
+        echo ${mkCheck "devShell" devShell}
+        echo works > $out
+      '';
 
-        packageName = "static-ls";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-          };
-
-        packages.default = self.packages.${system}.${packageName};
-        defaultPackage = self.packages.${system}.default;
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            haskellPackages.haskell-language-server # you must build it with your ghc to work
-            haskellPackages.fourmolu
-            haskellPackages.hiedb
-            sqlite
-            ghcid
-            cabal-install
-            hpack
-          ];
-          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
-          shellHook = "PS1=\"[static-ls:\\w]$ \"";
-        };
-        devShell = self.devShells.${system}.default;
-      });
+      # Other flake outputs provided by haskellNix can be accessed through
+      # this project output
+      inherit project;
+    });
 }
